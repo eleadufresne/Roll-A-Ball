441f3cd52406666cc2dfc151592ee54ca94bfb52
eleadufresne <elea.dufresne@mail.mcgill.ca>
2021-01-04 13:52:09 -0500

    First commit

---
+++ Roll-a-Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    // LateUpdate after every other update
+    void LateUpdate()
+    {
+        //makes the camera follow the position of the player (but not rotation)
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0.0f;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public GameObject restartTextObject;
+    public GameObject missingTextObject;
+
+    private Rigidbody rb;
+    private int count; //score
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+        restartTextObject.SetActive(false);
+        missingTextObject.SetActive(false);
+    }
+    void OnMove(InputValue movementValue)
+    {//function body
+
+        //Vector2 data from the movement value
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count == 27)
+        {
+            missingTextObject.SetActive(true);
+        }
+        if (count >= 28)
+        {
+            missingTextObject.SetActive(false);
+            winTextObject.SetActive(true);
+            restartTextObject.SetActive(true);
+        }
+    }
+    
+    //FixedUpdate when we use forces
+    void FixedUpdate() 
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        //makes it so we can adjust the speed for the Unity editor
+        rb.AddForce(movement * speed);
+    }
+    //To interact with pick up objects
+    void OnTriggerEnter(Collider other)
+    {
+        //If it is a collectible up object, deactivate it  
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            GetComponent<AudioSource>().Play();
+            ++count;
+            SetCountText();
+        }
+    }
+}
